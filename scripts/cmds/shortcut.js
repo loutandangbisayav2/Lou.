e87ed055396e80ec419a8d4016c0a1dafff6231d
Kaizenji <154584066+Kaizenji@users.noreply.github.com>
2024-02-22 19:00:35 +0800

    Update shortcut.js

--- scripts/cmds/shortcut.js
+++ scripts/cmds/shortcut.js
@@ -16,7 +16,7 @@ module.exports = {
 			vi: 'Thêm một phím tắt cho tin nhắn trong nhóm chat của bạn',
 			en: 'Add a shortcut for your message in group chat'
 		},
-		category: 'custom',
+		category: 'fun',
 		guide: {
 			vi: '   {pn} add <word> => <content>: thêm một phím tắt cho bạn (có thể gửi kèm hoặc phản hồi một tin nhắn có file để thêm tệp đính kèm)'
 				+ '\n   Ví dụ:\n    {pn} add hi => Xin chào mọi người'
