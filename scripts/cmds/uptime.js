8c4174bfb029eb2d020a708f32507d112c0309bd
Kaizenji <154584066+Kaizenji@users.noreply.github.com>
2024-02-25 11:46:09 +0800

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -1,1 +1,28 @@
-module.exports = { config: { name: "uptime", aliases: ["up", "upt"], version: "1.0", author: "OtinXSandip", role: 0, shortDescription: { en: "Displays the total number of users of the bot and check uptime " }, longDescription: { en: "Displays the total number of users who have interacted with the bot and check uptime." }, category: "system", guide: { en: "Type {pn}" } }, onStart: async function ({ api, event, args, usersData, threadsData }) { try { const allUsers = await usersData.getAll(); const allThreads = await threadsData.getAll(); const uptime = process.uptime(); const hours = Math.floor(uptime / 3600); const minutes = Math.floor((uptime % 3600) / 60); const seconds = Math.floor(uptime % 60); const uptimeString = `${hours}Hrs ${minutes}min ${seconds}sec`; api.sendMessage(`»「UPTIME」«\n\n⏰ | Running Time:\n❯» [${uptimeString}]\n\n🚸 | Total Users: ${allUsers.length}\n\n📊 | Total Threads: ${allThreads.length}`, event.threadID); } catch (error) { console.error(error); api.sendMessage("An error occurred while retrieving data.", event.threadID); } } };
+module.exports = {
+	config: {
+		name: "uptime",
+		aliases: ["up", "upt"],
+		version: "1.0",
+		author: "XyryllPanget",
+		role: 0,
+		shortDescription: {
+			en: "Displays the uptime of the bot."
+		},
+		longDescription: {
+			en: "Displays the amount of time that the bot has been running for."
+		},
+		category: "System",
+		guide: {
+			en: "Use {p}uptime to display the uptime of the bot."
+		}
+	},
+	onStart: async function ({ api, event, args }) {
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
+		api.sendMessage(`» Hello master, the bot has been running for\n${uptimeString}.`, event.threadID);
+	}
+};
