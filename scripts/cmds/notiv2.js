913da5af1aeb7fefca27bef8b3b4189d94c57373
Kaizenji <154584066+Kaizenji@users.noreply.github.com>
2024-02-26 20:51:54 +0800

    Update notiv2.js

--- scripts/cmds/notiv2.js
+++ scripts/cmds/notiv2.js
@@ -47,7 +47,7 @@ module.exports = {
     if (!args[0])
       return message.reply(getLang("missingMessage"));
     const formSend = {
-      body: `${getLang("notification")}\n ━━━━━ ◈\n\n✏${args.join(" ")}\n\n━━━━━ ◈\n━「 𝐊𝐚𝐢𝐳𝐞𝐧𝐣𝐢 」━`,
+      body: `${getLang("notification")}\n ━━━━━ ◈\n\n》${args.join(" ")}\n\n━━━━━ ◈\n━「 𝐊𝐚𝐢𝐳𝐞𝐧𝐣𝐢 」━`,
       attachment: await getStreamsFromAttachment(
         [
           ...event.attachments,
