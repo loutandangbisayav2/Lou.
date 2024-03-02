848d0ba78332f81794358824c9a92111566b4632
Kaizenji <154584066+Kaizenji@users.noreply.github.com>
2024-03-02 10:21:07 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -6,7 +6,7 @@ module.exports = {
     version: "1.0",
     author: "OtinXShiva",
     countDown: 5,
-    role: 2,
+    role: 1,
     shortDescription: "Send bot script",
     longDescription: "Send bot specified file ",
     category: "owner",
