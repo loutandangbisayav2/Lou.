737809979415684aba21d4ca9c3de19cf369fff3
Kaizenji <154584066+Kaizenji@users.noreply.github.com>
2024-03-03 12:09:19 +0800

    Update noti.js

--- scripts/cmds/noti.js
+++ scripts/cmds/noti.js
@@ -7,7 +7,7 @@ module.exports = {
     version: "1.7",
     author: "NTKhang / Aesther",
     countdown: 5,
-    role: 1,
+    role: 3,
     shortDescription: {
       vi: "Gửi thông báo từ admin đến tất cả các box",
       en: "Send notification from admin to all boxes",
