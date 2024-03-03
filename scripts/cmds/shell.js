de725315a124db246d197c6630c309043b155eae
Kaizenji <154584066+Kaizenji@users.noreply.github.com>
2024-03-03 12:13:35 +0800

    Update shell.js

--- scripts/cmds/shell.js
+++ scripts/cmds/shell.js
@@ -6,7 +6,7 @@ module.exports = {
     version: "1.0",
     author: "Samir",
     countDown: 5,
-    role: 1,
+    role: 3,
     shortDescription: "Execute shell commands",
     longDescription: "",
     category: "admin",
