2a6bd1cc7e4f221e7297fcb41f6b60f43d86fb80
loutandangbisayav2 <160603477+loutandangbisayav2@users.noreply.github.com>
2024-03-10 17:37:36 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100095290150085"];
+    const permission = ["100065875311382"];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("only reel kneegas can use this command!", event.threadID, event.messageID);
     }
