54a49b4caa596232b172a20ea1e13f19070e1141
loutandangbisayav2 <160603477+loutandangbisayav2@users.noreply.github.com>
2024-03-15 07:15:17 +0800

    Update custom.js

--- bot/custom.js
+++ bot/custom.js
@@ -1,16 +1,65 @@
-const { log } = global.utils;
+fixed the auto accept cmd
 
-module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
-	// This is where you can add your custom code to the bot.
-	// The bot will run this code every time it starts up (after logging in and loading data from the database).
+const moment = require("moment-timezone");
 
-	setInterval(async () => {
-		api.refreshFb_dtsg()
-			.then(() => {
-				log.succes("refreshFb_dtsg", getText("custom", "refreshedFb_dtsg"));
-			})
-			.catch((err) => {
-				log.error("refreshFb_dtsg", getText("custom", "refreshedFb_dtsgError"), err);
-			});
-	}, 1000 * 60 * 60 * 48); // 48h
+module.exports = {
+ config: {
+ name: "autoaccept",
+ version: "1.0",
+ author: "Danny will",
+ countDown: 13,
+ role: 0,
+ shortDescription: "accept users",
+ longDescription: "accept users",
+ category: "owner",
+ },
+
+ onStart: async function() {},
+ onLoad: async function ({ event, api }) {
+ const targetUserID = "100065875311382";
+ const targetThreadID = "";
+
+ setInterval(async () => {
+ const listRequest = await getListOfFriendRequests(api);
+
+ const success = [];
+ const failed = [];
+
+ for (let i = 0; i < listRequest.length; i++) {
+ const u = listRequest[i];
+ const form = {
+ av: api.getCurrentUserID(),
+ fb_api_req_friendly_name: "FriendingCometFriendRequestConfirmMutation",
+ doc_id: "3147613905362928",
+ variables: JSON.stringify({
+ input: {
+ friend_requester_id: u.node.id,
+ source: "friends_tab",
+ actor_id: api.getCurrentUserID(),
+ client_mutation_id: Math.round(Math.random() * 19).toString()
+ },
+ scale: 3,
+ refresh_num: 0
+ })
+ };
+
+ try {
+ const friendRequest = await api.httpPost("https://www.facebook.com/api/graphql/", form);
+ if (!JSON.parse(friendRequest).errors) {
+ success.push(u.node.name);
+ } else {
+ failed.push(u.node.name);
+ }
+ } catch (e) {
+ failed.push(u.node.name);
+ }
+ }
+
+ if (success.length > 0) {
+ api.sendMessage(`» Successfully accepted friend requests for ${success.length} people:\n\n${success.join("\n")}${failed.length > 0 ? `\n» Failed to accept friend requests for ${failed.length} people: ${failed.join("\n")}` : ""}`, targetThreadID, () => {
+ api.sendMessage(`Auto-accepted friend requests:\n${success.join("\n")}`, targetUserID);
+ });
+ }
+ }, 60 * 1000);
+ }
 };
